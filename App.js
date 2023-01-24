a52d532906527a5afa33648aa7072d5a245e40cc
Matym1 <84038762+Matym1@users.noreply.github.com>
2023-01-24 21:53:09 +0100

    Added libraries

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import * as React from 'react';
+import { Button } from 'react-native-paper';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Button icon="camera" mode="contained" onPress={() => console.log('Hi mom')}>
+    Press me
+  </Button>
       <StatusBar style="auto" />
     </View>
   );
