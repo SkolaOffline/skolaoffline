212a947cc49b9164659e23568661c945cf9b1f47
DaalbuCZ <buresd@gytool.cz>
2023-01-24 21:59:38 +0100

    Add Button

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
+        <Button icon="camera" mode="contained-tonal" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
