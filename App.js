d4fe07c7715a3a7ca028020f4d98ee90d6ba2548
Matym1 <84038762+Matym1@users.noreply.github.com>
2023-01-27 21:29:07 +0100

    Added react navigation

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
+
   return (
 
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-        <Button icon="camera" mode="contained-tonal" onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-      <StatusBar style="auto" />
+        
     </View>
   );
 }
