2d70e7fcc72616b3a05fa6267d5d85c58fdd8950
BuildTools <unconfigured@null.spigotmc.org>
2024-02-28 22:06:45 +0100

    Added missing API routes

--- app/Component/Shared/APIRoutes.js
+++ app/Component/Shared/APIRoutes.js
@@ -101,3 +101,179 @@ export class GetUserData extends APIRoute{
 
     }
 }
+
+export class GetNotifications extends APIRoute{
+
+
+    Build(){
+
+    this.url = 'https://<instance.url>/solapi/api/v1/user/notifications';
+
+    this.method ='GET';
+
+    this.headers.append('Authorization',`Bearer ${this.auth_token}`);
+
+    return this;
+
+    }
+}
+
+export class GetTimetableInfo extends APIRoute {
+    Build() {
+        this.url = 'https://<instance.url>/solapi/api/v1/timeTable/codeLists';
+
+        this.method = 'GET';
+
+        this.headers.append('Authorization', `Bearer ${this.auth_token}`);
+        
+        return this;
+    }
+}
+
+export class GetTimetable extends APIRoute{
+    Build(){
+        this.url = 'https://<instance.url>/solapi/api/v1/timeTable'
+
+        this.method = 'GET'
+
+        this.headers.append('Authorization',`Bearer ${this.auth_token}`);
+
+        this.query_params.append('StudentId', this.student_id);
+        this.query_params.append('DateFrom', this.date_from);
+        this.query_params.append('DateTo', this.date_to);
+        this.query_params.append('SchoolYearId', this.semester_id);
+
+        return this;
+    }
+}
+
+export class GetGrades extends APIRoute {
+    Build() {
+        this.url = `https://<instance.url>/solapi/api/v1/students/${this.student_id}/marks/list`;
+
+        this.method = 'GET';
+
+        this.headers.append('Authorization', `Bearer ${this.auth_token}`);
+
+        this.query_params.append('SigningFilter', this.signing_filter || 'all');
+        this.query_params.append('Pagination.PageNumber', this.page_number || 1);
+        this.query_params.append('Pagination.PageSize', this.page_size || 10);
+        this.query_params.append('SemesterId', this.semester_id);
+        if (this.subject_id) {
+            this.query_params.append('SubjectId', this.subject_id);
+        }
+        
+        return this;
+    }
+}
+
+export class GetGrade extends APIRoute{
+    Build(){
+        this.url = 'https://<instance.url>/solapi/api/v1/student/marks/' + this.grade_id
+
+        this.method = 'GET'
+
+        this.headers.append('Authorization',`Bearer ${this.auth_token}`);
+
+        this.query_params.append('StudentId', this.student_id);
+
+        return this;
+    }
+}
+
+export class GetCertificates extends APIRoute {
+    Build() {
+        this.url = `https://<instance.url>/solapi/api/v1/students/${this.student_id}/marks/final`;
+
+        this.method = 'GET';
+
+        this.headers.append('Authorization', `Bearer ${this.auth_token}`);
+
+        return this;
+    }
+}
+
+export class GetHomework extends APIRoute {
+    Build() {
+        this.url = `https://<instance.url>/solapi/api/v1/students/${this.student_id}/homeworks`;
+
+        this.method = 'GET';
+
+        this.headers.append('Authorization', `Bearer ${this.auth_token}`);
+
+        this.query_params.append('Pagination.PageNumber', this.page_number || 1);
+        this.query_params.append('Pagination.PageSize', this.page_size || 10);
+        this.query_params.append('Filter', this.filter || 'active');
+
+        return this;
+    }
+}
+
+export class GetBehaviors extends APIRoute {
+    Build() {
+        this.url = `https://<instance.url>/solapi/api/v1/students/${this.student_id}/behaviors`;
+
+        this.method = 'GET';
+
+        this.headers.append('Authorization', `Bearer ${this.auth_token}`);
+
+        this.query_params.append('Pagination.PageNumber', this.page_number || 1);
+        this.query_params.append('Pagination.PageSize', this.page_size || 10);
+        this.query_params.append('RecordsFilter', this.records_filter || 'all');
+
+        return this;
+    }
+}
+
+export class GetMessageSettings extends APIRoute {
+    Build() {
+        this.url = 'https://<instance.url>/solapi/api/v1/messages/settings';
+
+        this.method = 'GET';
+
+        this.headers.append('Authorization', `Bearer ${this.auth_token}`);
+
+        return this;
+    }
+}
+
+export class MarkMessageAsRead extends APIRoute {
+    Build() {
+        this.url = `https://<instance.url>/solapi/api/v1/messages/${this.message_id}/mark-as-read`;
+
+        this.method = 'PUT';
+
+        this.headers.append('Authorization', `Bearer ${this.auth_token}`);
+
+        return this;
+    }
+}
+
+export class GetMessages extends APIRoute {
+    Build() {
+        this.url = 'https://<instance.url>/solapi/api/v1/messages/received';
+
+        this.method = 'GET';
+
+        this.headers.append('Authorization', `Bearer ${this.auth_token}`);
+
+        this.query_params.append('Pagination.PageNumber', this.page_number || 1);
+        this.query_params.append('Pagination.PageSize', this.page_size || 10);
+
+        return this;
+    }
+}
+
+export class GetMarksCodeLists extends APIRoute {
+    Build() {
+        this.url = 'https://<instance.url>/solapi/api/v1/marks/codeLists';
+
+        this.method = 'GET';
+
+        this.headers.append('Authorization', `Bearer ${this.auth_token}`);
+
+        this.query_params.append('StudentId', this.student_id);
+        
+        return this;
+    }
+}
