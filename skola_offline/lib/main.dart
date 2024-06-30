8b05267921fe05d3ab548b93c54f3cf46e3ea4f9
Michael36500 <ambros.michael@seznam.cz>
2024-06-30 20:43:15 +0200

    basic layout and started working on login

--- skola_offline/lib/main.dart
+++ skola_offline/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+// import 'package:http/http
+//.dart' as http;
 
-void main() => runApp(MyApp());
-
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -116,40 +121,106 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class ProfileScreenState extends State<ProfileScreen> {
+  TextEditingController _usernameController = TextEditingController(); // Add this line
+  TextEditingController _passwordController = TextEditingController(); // Add this line
+
+  Future<void> login(username, password) async {
+    final storage = FlutterSecureStorage();
+    print('Username: $username');
+    await storage.write(key: 'username', value: username);
+    await storage.write(key: 'password', value: password);
+
+    // final response = await http.post(
+    //   Uri.parse('https://aplikace.skolaonline.cz/solapi/api/connect/token'),
+    //   headers: <String, String>{
+    //     'Content-Type': 'application/x-www-form-urlencoded',
+    //   },
+    //   body: <String, String>{
+    //     'grant_type': 'password',
+    //     'username': username,
+    //     'password': password,
+    //     'client_id': 'test_client',
+    //     'scope': 'openid offline_access profile sol_api',
+    //   },
+    // );
+
+  //   if (response.statusCode == 400) {
+  //     showDialog(
+  //       context: context,
+  //       builder: (BuildContext context) {
+  //         return AlertDialog(
+  //           title: Text('Wrong credentials'),
+  //           content: Text('Please check your username and password.'),
+  //           actions: [
+  //             TextButton(
+  //               onPressed: () {
+  //                 Navigator.of(context).pop();
+  //               },
+  //               child: Text('OK'),
+  //             ),
+  //           ],
+  //         );
+  //       },
+  //     );
+  //   }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CircleAvatar(
-                  radius: 30,
-                  child: Icon(Icons.person),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: CircleAvatar(
+                    radius: 30,
+                    child: Icon(Icons.person),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('John Doe'),
+                  ],
+                ),
+              ],
+            ),
+            TextField(
+              controller: _usernameController, // Add this line
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            TextField(
+              controller: _passwordController, // Add this line
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: () {
+                String username = _usernameController.text;
+                String password = _passwordController.text;
+                login(username, password);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.login),
+                    Text('Login'),
+                  ],
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('John Doe'),
-                ],
-              ),
-            ],
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Username',
             ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Password',
-            ),
-          ),
-
-        ],
+          ],
+        ),
       ),
     );
   }
