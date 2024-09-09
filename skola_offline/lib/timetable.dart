78aa16cbf339533302c23067b0175bb59e8f8f6c
Matym1 <84038762+Matym1@users.noreply.github.com>
2024-09-09 20:36:18 +0200

    Fixed broken default timetable
    The fix is TERRRIBLE, but it works...

--- skola_offline/lib/timetable.dart
+++ skola_offline/lib/timetable.dart
@@ -6,22 +6,26 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class TimetableScreenState extends State<TimetableScreen> {
   bool isDayScreen = true;
+  // ! THIS REALLY SUCKS, THERE IS A BETTER WAY, I AM BRAINDEAD. 
+  bool hasChanged = false;
 
   setDayScreen() {
     setState(() {
       isDayScreen = true;
+      hasChanged = true;
     });
   }
 
   setWeekScreen() {
     setState(() {
       isDayScreen = false;
+      hasChanged = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    if(MyApp.of(context)?.getDefaultToWeeklyTimetable() == true) setWeekScreen();
+    if(MyApp.of(context)?.getDefaultToWeeklyTimetable() == true && hasChanged==false) setWeekScreen();
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.timetable),
