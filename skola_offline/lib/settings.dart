fc191a8ac7c4d0fb15524c38f26a62ee2e3c1f1b
Matym1 <84038762+Matym1@users.noreply.github.com>
2024-09-10 12:47:38 +0200

    import cleanup

--- skola_offline/lib/settings.dart
+++ skola_offline/lib/settings.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:skola_offline/main.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'dart:convert';
-import 'package:skola_offline/app_settings.dart';
 
 
 
